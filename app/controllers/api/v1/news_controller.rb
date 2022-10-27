ec69e40293883533893b2f92227346cad0209dd7
Andrew Cheng <andrew.young.cheng@gmail.com>
2022-10-27 16:21:50 -0400

    Wired in the ability to send a query parameter to the Rails back end

--- app/controllers/api/v1/news_controller.rb
+++ app/controllers/api/v1/news_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::NewsController < ApplicationController
 
   def get_news
     n = News.new("f8eb522c7f47469d9fc91df0859faeb8")
-    @news = n.get_sources(country: 'us', language: 'en')
+    @news = n.get_top_headlines(q: params[:query])
 
     render json: @news
   end
--- app/javascript/src/components/HelloWorld.vue
+++ app/javascript/src/components/HelloWorld.vue
@@ -1,17 +1,22 @@
 <template>
   <p>
+    <input v-model="query" placeholder="Query"/>
     <button @click="getNews()">Get News</button>
   <br>
     <table-simple>
       <thead variant="primary">
         <tr>
           <th>Name</th>
+          <th>Author</th>
+          <th>Published At</th>
           <th>Description</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="source in news">
           <td>{{ source.name }}</td>
+          <td>{{ source.author }}</td>
+          <td>{{ source.publishedAt }}</td>
           <td>{{ source.description }}</td>
         </tr>
       </tbody>
@@ -25,13 +30,16 @@ import { ref } from 'vue'
 export default {
   setup() {
     let news = ref([])
+    let query = ref('Bitcoin')
 
     function getNews() {
-      fetch('http://localhost:3000/api/v1/get_news')
+      fetch('http://localhost:3000/api/v1/get_news' + '?query=' + query.value)
           .then(response => response.json())
           .then(response => news.value = response);
+
+      console.log(query.value);
     }
-    return { getNews, news };
+    return { getNews, news, query };
   }
 }
 </script>
