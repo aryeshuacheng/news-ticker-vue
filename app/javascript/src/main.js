9c15954a04640186e31f736c974d3d4837fe5e46
Andrew Cheng <andrew.young.cheng@gmail.com>
2022-10-28 14:34:02 -0400

    Wired in: Automatic load of sources. Pass in sources to view news by outlet. Bootstrap styling.

--- app/javascript/src/main.js
+++ app/javascript/src/main.js
@@ -1,9 +1,14 @@
 import { createApp } from 'vue'
 import HelloWorld from '@/components/HelloWorld.vue'
+import BootstrapVue3 from 'bootstrap-vue-3'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
 export default () => {
     document.addEventListener('DOMContentLoaded', () => {
         const app = createApp(HelloWorld)
+        app.use(BootstrapVue3)
         app.mount('#news')
     })
 }
